ae3062f5df84c5b523e224bc9a27383966486d5d
Martin <tjuyangxu@gmail.com>
2020-07-23 14:30:19 -0500

    Added Modelling Version1

--- Data Scientist/Linear_model.py
+++ Data Scientist/Linear_model.py
@@ -1,6 +1,78 @@
-import matplotlib
-import numpy
-import pandas
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 
-if __name__ == "__main__":
-    pass# to be updated with mathematical codes, expected outputs：1） Pearson Correlation; 2) R^2; 3) Model visualization
+data=pd.read_csv("Rig_Count_vs._Spot_Price.csv")
+data['Date'] = pd.to_datetime(data['Date'])
+print(data.head(2))
+Oil_Price_y = data["Oil_Price"][:, np.newaxis]
+Rig_count_x= data["Rig_count"][:, np.newaxis]
+model = LinearRegression().fit(Rig_count_x, Oil_Price_y)
+r_sq = model.score(Rig_count_x, Oil_Price_y)
+print('coefficient of determination:', r_sq)
+print('intercept:', model.intercept_)
+print('slope:', model.coef_)
+Oil_Price_y_pred = model.predict(Rig_count_x)
+data.plot.scatter("Rig_count","Oil_Price",label='Data')
+plt.plot(Rig_count_x, Oil_Price_y_pred, color='red', label='Linear regression')
+plt.title("Oil price vs Rig count")
+plt.grid()
+plt.legend()
+plt.show()
+
+#data selected from an specific date
+desire_date='01/01/2010'
+x=datetime.strptime(desire_date, '%m/%d/%Y')
+data_2010_2020=data[data['Date']>=x]
+
+Oil_Price_y = data_2010_2020["Oil_Price"][:, np.newaxis]
+Rig_count_x= data_2010_2020["Rig_count"][:, np.newaxis]
+model = LinearRegression().fit(Rig_count_x, Oil_Price_y)
+r_sq = model.score(Rig_count_x, Oil_Price_y)
+print('coefficient of determination:', r_sq)
+print('intercept:', model.intercept_)
+print('slope:', model.coef_)
+Oil_Price_y_pred = model.predict(Rig_count_x)
+data_2010_2020.plot.scatter("Rig_count","Oil_Price",label='Data')
+plt.plot(Rig_count_x, Oil_Price_y_pred, color='red',label='Linear regression')
+plt.title("Oil price vs Rig count 2010-2020")
+plt.grid()
+plt.legend()
+plt.show()
+
+Oil_Price_y_train = data_2010_2020["Oil_Price"][:-30][:, np.newaxis]
+Oil_Price_y_test = data_2010_2020["Oil_Price"][-30:][:, np.newaxis]
+
+# Rig count training/testing sets
+Rig_count_x_train = data_2010_2020["Rig_count"][:-30][:, np.newaxis]
+Rig_count_x_test = data_2010_2020["Rig_count"][-30:][:, np.newaxis]
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+
+# Train the model
+regr.fit(Rig_count_x_train, Oil_Price_y_train)
+
+# Make predictions
+Oil_Price_y_predic = regr.predict(Rig_count_x_test)
+
+# The coefficients
+print('Coefficients:', regr.coef_)
+
+print('Mean squared error: %.2f'
+      % mean_squared_error(Oil_Price_y_test, Oil_Price_y_predic))
+# The coefficient of determination: 1 is perfect prediction
+print('Coefficient of determination: %.2f'
+      % r2_score(Oil_Price_y_test, Oil_Price_y_predic))
+
+# Plot outputs
+plt.scatter(Rig_count_x_test, Oil_Price_y_test,  color='skyblue',label='Data')
+plt.plot(Rig_count_x_test, Oil_Price_y_predic, color='red', linewidth=2,label='Linear regression')
+plt.xlabel('Rig count')
+plt.ylabel('Oil Price')
+plt.legend()
+plt.show()
---
+++ Data Scientist/Polynomial.py
@@ -0,0 +1,39 @@
+import operator
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import PolynomialFeatures
+
+data=pd.read_csv("Rig_Count_vs._Spot_Price.csv")
+data['Date'] = pd.to_datetime(data['Date'])
+
+Oil_Price_y = data["Oil_Price"][:, np.newaxis]
+Rig_count_x= data["Rig_count"][:, np.newaxis]
+
+polynomial_features= PolynomialFeatures(degree=2)
+x_poly = polynomial_features.fit_transform(Rig_count_x)
+
+model = LinearRegression()
+model.fit(x_poly, Oil_Price_y)
+y_poly_pred = model.predict(x_poly)
+
+rmse = np.sqrt(mean_squared_error(Oil_Price_y,y_poly_pred))
+r2 = r2_score(Oil_Price_y,y_poly_pred)
+print('Mean squared error:',rmse)
+print('coefficient of determination:', r2)
+
+plt.scatter(Rig_count_x, Oil_Price_y, s=10,label='Data')
+plt.xlabel('Rig count')
+plt.ylabel('Oil Price')
+
+sort_axis = operator.itemgetter(0)
+sorted_zip = sorted(zip(Rig_count_x,y_poly_pred), key=sort_axis)
+Rig_count_x, y_poly_pred = zip(*sorted_zip)
+plt.plot(Rig_count_x, y_poly_pred, color='m', label='Polynomial regression')
+plt.title("Oil price vs Rig count")
+plt.grid()
+plt.legend()
+plt.show()
