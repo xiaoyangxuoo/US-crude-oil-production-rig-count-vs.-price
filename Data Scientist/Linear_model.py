ffbd43333fa9c887de905cde81e23e44368b6962
Martin <tjuyangxu@gmail.com>
2020-08-02 10:37:31 -0500

    Added figure list for final report  v2

--- Data Scientist/Linear_model.py
+++ Data Scientist/Linear_model.py
@@ -7,39 +7,14 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn import linear_model
 from pathlib import Path
 
-Datapath = Path(__file__).parent / "../data_list/Rig_Count_vs._Spot_Price.csv"
+Datapath = Path(__file__).parent / "../data_list/Rig_CountvsOil_price_v1.csv"
 data=pd.read_csv(Datapath)
-
-#############Old data model containing all data axes
-# print(data.head(2))
-#Oil_Price_y = data["Oil_Price"][:, np.newaxis]
-#Rig_count_x= data["Rig_count"][:, np.newaxis]
-# model = LinearRegression().fit(Rig_count_x, Oil_Price_y)
-# r_sq = model.score(Rig_count_x, Oil_Price_y)
-# print('coefficient of determination:', r_sq)
-# print('intercept:', model.intercept_)
-# print('slope:', model.coef_)
-# Oil_Price_y_pred = model.predict(Rig_count_x)
-# data.plot.scatter("Rig_count","Oil_Price",label='Data')
-# plt.plot(Rig_count_x, Oil_Price_y_pred, color='red', label='Linear regression')
-# plt.title("Oil price vs Rig count")
-# plt.grid()
-# plt.legend()
-# plt.show()
-
-#data selected from an specific date， to make it
-desire_date=pd.Timestamp('01/01/2010')
 data['Date'] = pd.to_datetime(data['Date'])
-condition = data['Date'] > desire_date
-data_2010_2020=data[condition]
-
-
-#lower cluster
-data1 = data_2010_2020[data_2010_2020["Oil_Price"]<71]
-data2 = data_2010_2020[data_2010_2020["Oil_Price"]>=71]
-
-Oil_Price_x = data1["Oil_Price"][:, np.newaxis]
-Rig_count_y= data1["Rig_count"][:, np.newaxis]
+data1=data[data["Oil_Price"]<71]
+data2=data[data["Oil_Price"]>=71]
+#Lower Closter (data1)
+Oil_Price_x = data1["Oil_Price"][3:, np.newaxis]
+Rig_count_y= data1["Rig_count"][:-3, np.newaxis]
 model = LinearRegression().fit(Oil_Price_x,Rig_count_y)
 r_sq = model.score(Oil_Price_x,Rig_count_y )
 print("----------Lower Cluster------------")
@@ -55,8 +30,6 @@ plt.legend()
 plt.show()
 print("Stat. Summary","\n")
 print(data1.describe())
-
-
 #Upper Closter (data2)
 Oil_Price_x = data2["Oil_Price"][:, np.newaxis]
 Rig_count_y= data2["Rig_count"][:, np.newaxis]
@@ -77,8 +50,7 @@ print("Stat. Summary","\n")
 print(data2.describe())
 
 
-#Lower Cluster Prediction
-# Oil Price training/testing sets
+
 Rig_count_y_train = data1["Rig_count"][:-30][:, np.newaxis]
 Rig_count_y_test = data1["Rig_count"][-30:][:, np.newaxis]
 
@@ -114,7 +86,7 @@ plt.title("Predic. Oil price vs Rig count")
 plt.legend()
 plt.show()
 
-#Upper Cluster Prediction
+
 # Oil Price training/testing sets
 Rig_count_y_train = data2["Rig_count"][:-30][:, np.newaxis]
 Rig_count_y_test = data2["Rig_count"][-30:][:, np.newaxis]
@@ -150,54 +122,3 @@ plt.xlabel('Oil Price')
 plt.title("Predic. Oil price vs Rig count")
 plt.legend()
 plt.show()
-
-
-
-
-# Oil_Price_y = data_2010_2020["Oil_Price"][:, np.newaxis]
-# Rig_count_x= data_2010_2020["Rig_count"][:, np.newaxis]
-# model = LinearRegression().fit(Rig_count_x, Oil_Price_y)
-# r_sq = model.score(Rig_count_x, Oil_Price_y)
-# print('coefficient of determination:', r_sq)
-# print('intercept:', model.intercept_)
-# print('slope:', model.coef_)
-# Oil_Price_y_pred = model.predict(Rig_count_x)
-# data_2010_2020.plot.scatter("Rig_count","Oil_Price",label='Data')
-# plt.plot(Rig_count_x, Oil_Price_y_pred, color='red',label='Linear regression')
-# plt.title("Oil price vs Rig count 2010-2020")
-# plt.grid()
-# plt.legend()
-# plt.show()
-#
-# Oil_Price_y_train = data_2010_2020["Oil_Price"][:-30][:, np.newaxis]
-# Oil_Price_y_test = data_2010_2020["Oil_Price"][-30:][:, np.newaxis]
-#
-# # Rig count training/testing sets
-# Rig_count_x_train = data_2010_2020["Rig_count"][:-30][:, np.newaxis]
-# Rig_count_x_test = data_2010_2020["Rig_count"][-30:][:, np.newaxis]
-#
-# # Create linear regression object
-# regr = linear_model.LinearRegression()
-#
-# # Train the model
-# regr.fit(Rig_count_x_train, Oil_Price_y_train)
-#
-# # Make predictions
-# Oil_Price_y_predic = regr.predict(Rig_count_x_test)
-#
-# # The coefficients
-# print('Coefficients:', regr.coef_)
-#
-# print('Mean squared error: %.2f'
-#       % mean_squared_error(Oil_Price_y_test, Oil_Price_y_predic))
-# # The coefficient of determination: 1 is perfect prediction
-# print('Coefficient of determination: %.2f'
-#       % r2_score(Oil_Price_y_test, Oil_Price_y_predic))
-#
-# # Plot outputs
-# plt.scatter(Rig_count_x_test, Oil_Price_y_test,  color='skyblue',label='Data')
-# plt.plot(Rig_count_x_test, Oil_Price_y_predic, color='red', linewidth=2,label='Linear regression')
-# plt.xlabel('Rig count')
-# plt.ylabel('Oil Price')
-# plt.legend()
-# plt.show()
